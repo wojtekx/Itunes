d911c68f6f4b9048cd105d8a32841663e0772d54
Wojtek <woltorb8@gmail.com>
2018-10-02 10:34:32 +0200

    no message

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './kids.png';
 import './App.css';
 
 
@@ -8,36 +8,45 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      songs: [],
+      song: [{}],
     };
   }
 
   componentDidMonth() {
 
   };
+  submit = () => {
+    console.log(this.input.value);
+  }
 
-  musicResults = document.getElementById("musicResults");
-
-  getMusic = () => {
-    fetch('https://itunes.apple.com/search?term=jack+johnson&limit=6', {
+  getMusic = (el) => {
+    el.preventDefault();
+    fetch(`https://itunes.apple.com/search?term=${this.input.value}&limit=6`, {
       method: 'GET',
     }).then(r => r.json())
-      .then(data => console.log(data.results[0]) || this.setState({
-        songs: data.results,
+      .then(data => console.log(data.results) || this.setState({
+        song: data.results,
       }))
+
 
   }
 
   render() {
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Itunes in React</h1>
         </header>
-        <button onClick={this.getMusic} >Klik</button>
-
+        <form>
+          <input type="text" ref={input => this.input = input} placeholder="Search songs"></input>
+          <button onClick={this.getMusic} >Search</button>
+        </form>
+        <div id="songs">
+          {this.state.song.map(el => <div>{el.artistName}</div>)}
+          {this.state.song.map(el => <div>{el.trackName}</div>)}
+          {this.state.song.map(el => <img src={el.artworkUrl100}></img>)}
+        </div>
       </div>
 
     );
